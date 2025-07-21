81fa7900089b02956f68bf9be79bc6a0796b0a5e
Udit Singh <2004udit@gmail.com>
2025-07-21 20:10:53 +0530

    SignIn Test

---
+++ Sign_in_page/test_Sign1.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from test_sign2 import personal_info
+
+driver = webdriver.Edge()
+
+driver.get("https://automationexercise.com/")
+driver.maximize_window()
+driver.implicitly_wait(2)
+
+driver.find_element(By.CSS_SELECTOR, "a[href='/login']").click()
+driver.find_element(By.XPATH, "//input[@name='name']").send_keys("Udit Singh")
+driver.find_element(By.CSS_SELECTOR, "input[data-qa='signup-email']").send_keys("2004udit@gmail.com")
+
+driver.find_element(By.CSS_SELECTOR, "button[data-qa='signup-button']").click()
+wait = WebDriverWait(driver, 2)
+
+personal_info(driver)
+
+driver.find_element(By.CSS_SELECTOR, "a[href='/delete_account']").click()
+print(driver.find_element(By.CSS_SELECTOR, "h2[data-qa='account-deleted'] b").text)
+driver.find_element(By.XPATH, "//a[@data-qa='continue-button']").click()
+
---
+++ Sign_in_page/test_sign2.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def personal_info(driver):
+    # personal info register(new page login 2)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='id_gender1']")))
+    driver.find_element(By.CSS_SELECTOR, "input[id='id_gender1']").click()
+    driver.find_element(By.CSS_SELECTOR, "#password").send_keys("TESTINGhere")
+    driver.find_element(By.XPATH, "//select[@id='days']/option[@value='10']").click()
+    driver.find_element(By.CSS_SELECTOR, "select[id='months'] option[value='2']").click()
+    driver.find_element(By.CSS_SELECTOR, "select[name='years'] option[value='2004']").click()
+    driver.find_element(By.CSS_SELECTOR, "#newsletter").click()
+    driver.find_element(By.CSS_SELECTOR, "#optin").click()
+    driver.find_element(By.CSS_SELECTOR, "#first_name").send_keys("Udit")
+    driver.find_element(By.XPATH, "//input[@name='last_name']").send_keys("Singh")
+    driver.find_element(By.NAME, "company").send_keys("Fresher")
+    driver.find_element(By.CSS_SELECTOR, "#country").send_keys("I")
+    driver.find_element(By.XPATH, "//select[@id='country']/option[@value='India']").click()
+    driver.find_element(By.CSS_SELECTOR, "#city").send_keys("Aligarh")
+    driver.find_element(By.ID, "state").send_keys("Uttar Pradesh")
+    driver.find_element(By.XPATH, "//input[@name='zipcode']").send_keys("202001")
+    driver.find_element(By.CSS_SELECTOR, "#mobile_number").send_keys("9258192581")
+    driver.find_element(By.CSS_SELECTOR, "#address1").send_keys("Gandhi Nagar")
+    driver.find_element(By.XPATH, "//input[@id = 'address2']").send_keys("that's it")
+    driver.find_element(By.CSS_SELECTOR, "button[data-qa='create-account']").click()
+    print(driver.find_element(By.CSS_SELECTOR, "h2[class='title text-center'] b").text)
+    driver.find_element(By.XPATH,"//a[@class='btn btn-primary']").click()
