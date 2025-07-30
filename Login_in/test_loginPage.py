8da9900a5df98f296f5353a331adcb5ed8216ea0
Udit Singh <2004udit@gmail.com>
2025-07-30 17:05:54 +0530

    login

--- Login_in/test_loginPage.py
+++ Login_in/test_loginPage.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-from test_loginPage2 import logout
+#from test_loginPage2 import logout
 
 
 def login(driver):
@@ -9,10 +9,10 @@ def login(driver):
     driver.find_element(By.CSS_SELECTOR, "a[href='/login']").click()
 
     driver.find_element(By.XPATH, "//input[@data-qa='login-email']").send_keys("2004udit@gmail.com")
-    driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys("55AC$C7b")
+    driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys("as")
     driver.find_element(By.CSS_SELECTOR, "button[data-qa='login-button']").click()
 
-    logout(driver)
+    #logout(driver)
 
 # THIS is what pytest will recognize and run
 def test_login(browserInstance):
